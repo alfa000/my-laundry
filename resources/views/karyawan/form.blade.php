9a16cbf26ae1c2422d95cd4f5ba0be5ce3575078
ilham assidiq <ilhamassidiq282@gmail.com>
2022-07-18 20:54:07 +0700

    membuat form karyawan

---
+++ resources/views/karyawan/form.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.auth')
+
+@section('title', 'Register')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center align-items-center" style="min-height: calc(100vh - 107px)">
+        <div class="col-md-6 my-5">
+            <div class="text-center mb-2">
+                <img src="{{ asset('assets/img/logo.png') }}" width="100px" class="m-auto" alt="">
+            </div>
+            <h2 class="text-center mb-5">Karyawan</h2>
+
+        </div>
+    </div>
+</div>
+@endsection
